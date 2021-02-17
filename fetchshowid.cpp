dd5ee1d94933834d9202e5344347aada2300b186
Blagovest Petrov <blagovest@petrovs.info>
2021-02-18 01:10:05 +0200

    Changed logo with better quality image
    Signed-off-by: Blagovest Petrov <blagovest@petrovs.info>

--- fetchshowid.cpp
+++ fetchshowid.cpp
@@ -11,13 +11,5 @@ FetchShowID::FetchShowID()
 
 //int FetchShowID::Fetch() {
 //    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
-//    QObject::connect(manager, &QNetworkAccessManager::finished, this, [=](QNetworkReply *reply) {
-//        if (reply->error()) {
-//            qDebug() << reply->errorString();
-//            return;
-//        }
 
-//        QString answer = reply->readAll();
-
-//    }
 //}
